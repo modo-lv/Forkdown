94dcce88af9354bb7c979e012ac2f3d2c5629d7b
MoDo <modo@modo.lv>
2020-11-29 23:07:55 +0200

    Section meta text.

--- Forkdown.Core/Main/Build/Workers/MetaTextWorker.cs
+++ Forkdown.Core/Main/Build/Workers/MetaTextWorker.cs
@@ -4,16 +4,27 @@ using Forkdown.Core.Elements;
 namespace Forkdown.Core.Build.Workers {
   public class MetaTextWorker : Worker {
     public override Element BuildElement(Element element) {
-      if (element is Paragraph p && p.Subs.FirstOrDefault() is Code c && c.Content.StartsWith(":")) {
-        p.Kind = c.Content.Substring(1) switch {
-          {} s when s == "!" || s == "warn" => Element.Metadata.Warning,
-          {} s when s == "i" => Element.Metadata.Info,
-          {} s when s == "?" || s == "help" => Element.Metadata.Help,
-          _ => p.Kind
-        };
-        p.Subs.RemoveAt(0);
-        if (p.Subs.FirstOrDefault() is Text t)
-          t.Content = t.Content.TrimStart();
+      switch (element) {
+        case Paragraph p when p.Subs.FirstOrDefault() is Code c && c.Content.StartsWith(":"): {
+          p.Kind = c.Content.Substring(1) switch {
+            {} w when w == "!" || w == "warn" => Element.Metadata.Warning,
+            {} i when i == "i" || i == "info" => Element.Metadata.Info,
+            {} h when h == "?" || h == "help" => Element.Metadata.Help,
+            _ => p.Kind
+          };
+          p.Subs.RemoveAt(0);
+          if (p.Subs.FirstOrDefault() is Text t)
+            t.Content = t.Content.TrimStart();
+          break;
+        }
+        case Section s:
+          s.Kind = s switch {
+            {} w when w.Settings.IsTrue("warn") => Element.Metadata.Warning,
+            {} i when i.Settings.IsTrue("info") => Element.Metadata.Info,
+            {} h when h.Settings.IsTrue("help") => Element.Metadata.Help,
+            _ => element.Kind
+          };
+          break;
       }
       return element;
     }
