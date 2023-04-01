05c1aa9ffe19f7eeb312a4adfcd74d40e220b6f4
modo_lv <modo@modo.lv>
2023-04-02 00:50:53 +0300

    Changed Simpler.Net references to local and added output path to web projects.

--- Forkdown.Web/Project.cs
+++ Forkdown.Web/Project.cs
@@ -1,13 +1,22 @@
 ﻿using System.IO.Abstractions;
 
-namespace Forkdown.Web; 
+namespace Forkdown.Web;
 
 /// <summary>
 /// Extends a Forkdown <see cref="Core.Project"/> with metadata needed for compiling it to a website. 
 /// </summary>
 public class Project : Core.Project {
+
+  /// <summary>
+  /// Root directory of the generated output files.
+  /// </summary>
+  public readonly IDirectoryInfo Output;
+
   /// <inheritdoc />
-  public Project(IDirectoryInfo root) : base(root) {
-    
+  public Project(
+    IDirectoryInfo root,
+    IDirectoryInfo? output = null
+  ) : base(root) {
+    this.Output = output ?? root.FileSystem.DirectoryInfo.New("out-web");
   }
 };
--- Forkdown.Web/Wiring/Dependencies/Logging.cs
+++ Forkdown.Web/Wiring/Dependencies/Logging.cs
@@ -4,11 +4,14 @@ using Serilog;
 
 namespace Forkdown.Web.Wiring.Dependencies;
 
-public class Logging {
+/// <summary>
+/// Contains configuration for logg output. 
+/// </summary>
+public static class Logging {
   /// <summary>
   /// Configure logging for the app.
   /// </summary>
-  public static Action<ILoggingBuilder> Config = cfg => {
+  public static readonly Action<ILoggingBuilder> Config = cfg => {
     cfg.AddSerilog(new LoggerConfiguration()
       .ReadFrom
       .Configuration(new ConfigurationBuilder()
